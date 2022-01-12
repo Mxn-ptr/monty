0fd5732214d7393c2db47a978afd2eb3f864dd47
Neo <neochronos.chaos9@gmail.com>
2022-01-12 02:12:10 +0100

    Pushing back README and monty_main

---
+++ monty_main.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * main - main of the monty interpreter
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: EXIT_SUCCESS on success or EXIT_FAILURE on failure
+ */
+
+int main(int argc, char **argv)
+{
+	stack_t *stack = NULL;
+	int status = EXIT_SUCCESS;
+
+
+	if (argc != 2)
+		return (error_usage());
+	get_cmd(argv[1], &stack);
+	free_dlistint(stack);
+	return (status);
+}
